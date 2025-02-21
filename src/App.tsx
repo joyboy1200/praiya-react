d8ca2e547cc43dd073dd731d66ef6e4a97802913
joyboy1200 <yash@ariseall.com>
2025-02-21 17:55:23 +0530

    [App] React init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from 'react'
+import { MultiSelect } from 'primereact/multiselect'
 import './App.css'
 
 function App() {
 	const [value, setValue] = useState('')
 	const [error, setError] = useState('')
+	const [data, setData] = useState({})
+	const [selectedCities, setSelectedCities] = useState([])
+
+	const items = [
+		{ name: 'Alphabets', code: 'alphabets' },
+		{ name: 'Numbers', code: 'numbers' },
+		{ name: 'Highest Alphabet', code: 'highest_alphabet' },
+	]
 
 	const onSubmit = async () => {
 		try {
@@ -17,7 +26,7 @@ function App() {
 				body: JSON.stringify(parsedValue),
 			})
 
-			console.log(await res.json())
+			setData(await res.json())
 		} catch (error) {
 			setError('Invalid JSON')
 		}
@@ -38,7 +47,27 @@ function App() {
 			>
 				Submit
 			</button>
-			<button onClick={() => setValue('')}>Clear</button>
+			<button style={{ marginBottom: '12px' }} onClick={() => setValue('')}>
+				Clear
+			</button>
+
+			{selectedCities.map((x: any) => (
+				<div style={{ marginTop: '12px', marginBottom: '12px' }}>
+					{x.name}: {(data as any)[x.code]?.join(', ')}
+				</div>
+			))}
+
+			<div style={{ marginTop: '16px' }}></div>
+
+			<MultiSelect
+				value={selectedCities}
+				onChange={(e) => setSelectedCities(e.value)}
+				options={items}
+				optionLabel='name'
+				placeholder='Select Items'
+				maxSelectedLabels={3}
+				className='w-full md:w-20rem'
+			/>
 		</div>
 	)
 }

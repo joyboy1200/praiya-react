0643897604ef779742fd0fa661b03b200a5af26c
joyboy1200 <yash@ariseall.com>
2025-02-21 17:27:47 +0530

    [App] React init

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+	const [value, setValue] = useState('')
+	const [error, setError] = useState('')
+
+	const onSubmit = async () => {
+		try {
+			setError('')
+			const parsedValue = JSON.parse(value)
+			const res = await fetch('http://localhost:3000/bfhl', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(parsedValue),
+			})
+
+			console.log(await res.json())
+		} catch (error) {
+			setError('Invalid JSON')
+		}
+	}
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', width: '400px' }}>
+			<input
+				type='text'
+				value={value}
+				style={{ padding: '8px 12px', borderRadius: '8px' }}
+				onChange={(e) => setValue(e.target.value)}
+			></input>
+			{error && <p style={{ color: 'red' }}>{error}</p>}
+			<button
+				style={{ marginTop: '12px', marginBottom: '12px' }}
+				onClick={onSubmit}
+			>
+				Submit
+			</button>
+			<button onClick={() => setValue('')}>Clear</button>
+		</div>
+	)
+}
+
+export default App
